4291e849b3b85bc3d46d5a66c3b1d83b55bc70fb
Vic <victor.hytyk@gmail.com>
2015-02-27 13:07:32 +0200

    add build api call

--- SkypeBotWebApi/Controllers/SkypeApiController.cs
+++ SkypeBotWebApi/Controllers/SkypeApiController.cs
@@ -11,6 +11,19 @@ namespace SkypeBotWebApi.Controllers
 {
     public class SkypeController : ApiController
     {
+
+        [HttpGet]
+        public IHttpActionResult BuildEvent(string to, string job, string eventName, string revision=null, string node=null)
+        {
+            var rmqService = new RmqSkypeService();
+            rmqService.PushMessage(new RmqSkypeMessage
+            {
+                Conversation = to,
+                Message = string.Format("{0} build of {1} branch {2} for {3} node", job, revision, eventName, node)
+            });
+            return Ok();
+        }
+
         [HttpGet]
         public IHttpActionResult SendMessage(string to, string msg)
         {
