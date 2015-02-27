59b141d42a0a4ee34ff2a3be1f0437d2c9987e49
Vic <victor.hytyk@gmail.com>
2015-02-27 15:04:11 +0200

    "added on review" api call

--- SkypeBotWebApi/Controllers/SkypeApiController.cs
+++ SkypeBotWebApi/Controllers/SkypeApiController.cs
@@ -11,6 +11,36 @@ namespace SkypeBotWebApi.Controllers
 {
     public class SkypeController : ApiController
     {
+        private string GetSkypeNameByCruName(string cruName)
+        {
+            switch (cruName)
+            {
+                case "tkonyk":
+                    return "tkonyk.ja";
+                case "vhytyk":
+                    return "victor.hytyk.prl";
+                default:
+                    return cruName;
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult AddedOnReview(string reviewer, string cruid)
+        {
+            string skypeName = GetSkypeNameByCruName(reviewer);
+            if (string.IsNullOrWhiteSpace(skypeName))
+            {
+                return BadRequest("can't find skype name for " + reviewer);
+            }
+            var rmqService = new RmqSkypeService();
+            
+            rmqService.PushMessage(new RmqSkypeMessage
+            {
+                Conversation = GetSkypeNameByCruName(reviewer),
+                Message = string.Format("You were added on CR: http://crucible/cru/{0}", cruid)
+            });
+            return Ok();
+        }
 
         [HttpGet]
         public IHttpActionResult BuildEvent(string to, string job, string eventName, string revision=null, string node=null)
