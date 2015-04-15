80aa25e6efc8e5ccfc796665c6bac43a5b64ce54
Vic <victor.hytyk@gmail.com>
2015-04-15 17:33:31 +0300

    Revert "fix for chat bot"
    This reverts commit c268ef0d7df8816248a088b90e440ed45adbb520.

--- SkypeBot/BotEngine/ChatBotProvider.cs
+++ SkypeBot/BotEngine/ChatBotProvider.cs
@@ -8,28 +8,17 @@ namespace SkypeBot.BotEngine
 {
     public class ChatBotProvider : IChatBotProvider
     {
-        private DateTime lastBotResponse = DateTime.MinValue;
-
         private ChatterBotSession _chatterBot = null;
-
-        private void InitBot()
-        {
-            _chatterBot = new ChatterBotFactory().Create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477").CreateSession();
-        }
 
         public ChatBotProvider()
         {
-            InitBot();
+            _chatterBot = new ChatterBotFactory().Create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477").CreateSession();
         }
 
         public string Think(string message)
         {
             try
             {
-                if ((DateTime.Now - lastBotResponse).TotalMinutes > 5)
-                {
-                    InitBot();
-                }
                 return _chatterBot.Think(message);
             }
             catch (Exception)
