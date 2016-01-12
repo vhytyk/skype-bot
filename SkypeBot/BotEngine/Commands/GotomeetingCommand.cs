205f3b966319e7e4a6be59d7592b6e20e27b47b2
Vic Hytyk <victor.hytyk@justanswer.com>
2016-01-12 18:49:47 +0200

    g2m

---
+++ SkypeBot/BotEngine/Commands/GotomeetingCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SkypeBot.BotEngine.Commands
+{
+    class GotomeetingCommand : ISkypeCommand
+    {
+        public string RunCommand()
+        {
+            string URI = "https://api.citrixonline.com/G2MFREE/rest/v1/session";
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                string HtmlResult = wc.UploadString(URI, "");
+                Match idValueMatch = Regex.Match(HtmlResult, @"""webUrl"":""(.*?)""");
+                return idValueMatch.Groups[1].Value;
+            }
+
+        }
+
+        public void Init(string arguments)
+        {
+            
+        }
+    }
+}
